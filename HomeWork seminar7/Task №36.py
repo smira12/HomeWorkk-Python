62a0e67e22841cbd185f3058341b22a4e7e3ca0f
smira12 <m1ronsa89@gmail.com>
2023-03-28 03:11:48 +0300

    Add modified task 36 seminar 7

--- HomeWork seminar7/Task №36.py
+++ HomeWork seminar7/Task №36.py
@@ -16,7 +16,6 @@
 # 6 12 18 24 30 36
 
 
-
 def print_operation_table(operation, num_rows = 6, num_columns = 6):
     for x in range(1, num_rows + 1):
         for y in range(1, num_columns + 1):
@@ -24,16 +23,3 @@ def print_operation_table(operation, num_rows = 6, num_columns = 6):
         print()
 
 print_operation_table(lambda x, y: x * y)
-
-
-
-
-# def print_operation_table(operation, num_rows=9, num_columns=9):
-#     a = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
-#     for i in a:
-#         print(*[f"{x:>3}" for x in i])
-#
-#
-# print_operation_table(lambda x, y: x * y)
-
-
